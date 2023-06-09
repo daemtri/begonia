323e2008f24bfa53899196bbc8fefa1db01d34ba
daemtri <daemtri-molly@bianfeng.com>
2023-06-09 16:27:04 +0800

    move ig to ig

--- app/integrator.go
+++ app/integrator.go
@@ -1,10 +1,17 @@
 package app
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 
+	"git.bianfeng.com/stars/wegame/wan/wanx/bootstrap"
 	"git.bianfeng.com/stars/wegame/wan/wanx/contract"
 	"github.com/go-chi/chi/v5"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/proto"
 )
 
 type Integrator struct {
@@ -45,4 +52,72 @@ func (it *Integrator) integrate(mr *moduleRuntime) {
 		})
 	})
 	currentModule = nil
+}
+
+type GrpcServiceRegistrar interface {
+	contract.RouteRegistrar
+	grpc.ServiceRegistrar
+}
+
+type grpcServiceRegistrarImpl struct {
+	ci      *bootstrap.ContextInjector
+	route   contract.RouteRegistrar
+	service grpc.ServiceRegistrar
+}
+
+func newGrpcServiceRegistrarImpl(
+	rr *bootstrap.RouteRegistrar,
+	sr *bootstrap.ServiceRegistrar,
+	ci *bootstrap.ContextInjector,
+) (*grpcServiceRegistrarImpl, error) {
+	lsri := &grpcServiceRegistrarImpl{
+		route:   rr,
+		service: sr,
+		ci:      ci,
+	}
+	return lsri, nil
+}
+
+func (gr *grpcServiceRegistrarImpl) RegisterService(desc *grpc.ServiceDesc, impl any) {
+	mr := currentModule
+	gr.ci.Bind(desc.ServiceName, func(ctx context.Context) context.Context {
+		return withModuleRuntime(ctx, mr)
+	})
+	gr.service.RegisterService(desc, impl)
+}
+
+func Route[K ~int32, T proto.Message](msgID K, handleFunc func(ctx context.Context, req T) error) contract.RouteCell {
+	mr := currentModule
+	return contract.RouteCell{
+		MsgID: int32(msgID),
+		HandleFunc: func(ctx context.Context, req []byte) error {
+			var x T
+			v := x.ProtoReflect().New().Interface()
+			if req != nil {
+				if err := proto.Unmarshal(req, v); err != nil {
+					return status.Error(codes.InvalidArgument, fmt.Sprintf("message id %d does not match the message type, error %s", msgID, err))
+				}
+			}
+			return handleFunc(withModuleRuntime(ctx, mr), v.(T))
+		},
+	}
+}
+
+func (gr *grpcServiceRegistrarImpl) RegisterRoute(routes ...contract.RouteCell) {
+	gr.route.RegisterRoute(routes...)
+}
+
+type httpServerMux struct {
+	chi.Router
+}
+
+func newHttpServerMux() (*httpServerMux, error) {
+	return &httpServerMux{
+		Router: chi.NewRouter(),
+	}, nil
+}
+
+func (mux *httpServerMux) Enabled() bool {
+	logger.Info("http server enable check, routes", "routes", len(mux.Routes()))
+	return len(mux.Routes()) > 0
 }
--- app/module.go
+++ app/module.go
@@ -5,14 +5,7 @@ import (
 	"fmt"
 	"time"
 
-	"git.bianfeng.com/stars/wegame/wan/wanx/bootstrap"
-	"git.bianfeng.com/stars/wegame/wan/wanx/contract"
 	"git.bianfeng.com/stars/wegame/wan/wanx/di/box"
-	"github.com/go-chi/chi/v5"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
-	"google.golang.org/protobuf/proto"
 )
 
 var (
@@ -58,7 +51,7 @@ type Module interface {
 	// Init 模块初始化
 	Init(ctx context.Context) error
 	// Integrate 注册业务服务处理器
-	Integrate(it Integrator)
+	Integrate(ig Integrator)
 	// Destroy
 	Destroy(ctx context.Context) error
 }
@@ -68,72 +61,4 @@ func RegisterModule[T Module](name string, m T) {
 		panic(fmt.Sprintf("module %s already registered", name))
 	}
 	modules[name] = m
-}
-
-type GrpcServiceRegistrar interface {
-	contract.RouteRegistrar
-	grpc.ServiceRegistrar
-}
-
-type grpcServiceRegistrarImpl struct {
-	ci      *bootstrap.ContextInjector
-	route   contract.RouteRegistrar
-	service grpc.ServiceRegistrar
-}
-
-func newGrpcServiceRegistrarImpl(
-	rr *bootstrap.RouteRegistrar,
-	sr *bootstrap.ServiceRegistrar,
-	ci *bootstrap.ContextInjector,
-) (*grpcServiceRegistrarImpl, error) {
-	lsri := &grpcServiceRegistrarImpl{
-		route:   rr,
-		service: sr,
-		ci:      ci,
-	}
-	return lsri, nil
-}
-
-func (gr *grpcServiceRegistrarImpl) RegisterService(desc *grpc.ServiceDesc, impl any) {
-	mr := currentModule
-	gr.ci.Bind(desc.ServiceName, func(ctx context.Context) context.Context {
-		return withModuleRuntime(ctx, mr)
-	})
-	gr.service.RegisterService(desc, impl)
-}
-
-func Route[K ~int32, T proto.Message](msgID K, handleFunc func(ctx context.Context, req T) error) contract.RouteCell {
-	mr := currentModule
-	return contract.RouteCell{
-		MsgID: int32(msgID),
-		HandleFunc: func(ctx context.Context, req []byte) error {
-			var x T
-			v := x.ProtoReflect().New().Interface()
-			if req != nil {
-				if err := proto.Unmarshal(req, v); err != nil {
-					return status.Error(codes.InvalidArgument, fmt.Sprintf("message id %d does not match the message type, error %s", msgID, err))
-				}
-			}
-			return handleFunc(withModuleRuntime(ctx, mr), v.(T))
-		},
-	}
-}
-
-func (gr *grpcServiceRegistrarImpl) RegisterRoute(routes ...contract.RouteCell) {
-	gr.route.RegisterRoute(routes...)
-}
-
-type httpServerMux struct {
-	chi.Router
-}
-
-func newHttpServerMux() (*httpServerMux, error) {
-	return &httpServerMux{
-		Router: chi.NewRouter(),
-	}, nil
-}
-
-func (mux *httpServerMux) Enabled() bool {
-	logger.Info("http server enable check, routes", "routes", len(mux.Routes()))
-	return len(mux.Routes()) > 0
 }
